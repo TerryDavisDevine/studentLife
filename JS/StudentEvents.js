54915af03be8a2735e78c9b4e5ec397447524061
TerryDavisDevine <oneill-f27@ulster.ac.uk>
2026-03-24 11:26:59 +0000

    added functionality to student events, added a header and a footer to all pages, created a resources folder to hold JSON/data and images

--- JS/StudentEvents.js
+++ JS/StudentEvents.js
@@ -1,20 +1,45 @@
-// get events 
+//get the events from the json file
 async function getEvents(){
-    try{
-      let jsonStuff = await fetch("../resources/events.json");
-      let events = await jsonStuff.json();
-      return events;
-    }catch(error){
-      console.error("Error, can't proccess event data: ", error);
-    }
+  try{
+    let response = await fetch("../resources/events.json");
+    let text = await response.text();
+    let data = JSON.parse(text);
+    return data;
+  }catch(error){
+    console.error("ERROR:", error);
+  }
 }
-function createEvent(name,date,time,location,desc){
-  return document.createElement("h6").appendChild(document.createTextNode(name));
+//create the inidivudal elemtns for the events
+function createElement(type,content){
+  let node = document.createElement(type);
+  node.innerHTML=content;
+  return node
 }
+//create the elemtns for the events
+function createEvent(details){
+  let div = document.createElement("div");
+  div.setAttribute("class","event");
+  div.appendChild(createElement("h3",details.Name));
+  div.appendChild(createElement("h4",details.Date));
+  div.appendChild(createElement("h4",details.Time));
+  div.appendChild(createElement("h4",details.Location));
+  div.appendChild(createElement("h4",details.Desc));
+  return div;
+}
+//add elements to the page
 function showEvents(events){
+    let allEvents = events.events;
+    console.log(allEvents.length);
     let eventSpace = document.getElementById("eventSpace");
-    for(let i =0; i<events.length;i++){
-      eventSpace.appendChild(createEvent)
-    }
+    for(let i =0; i<allEvents.length;i++){
+      console.log("HI");
+      eventSpace.appendChild(createEvent(allEvents[0]));
+   }
 }
-showEvents(getEvents());
+//waits for getEvents to actually give the data
+async function init() {
+  const events = await getEvents();
+  showEvents(events);
+}
+
+init();
